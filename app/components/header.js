853acb157c96f2d9804a3754bdee53d89f7e6226
cathysunkc <cathysun2012@yahoo.com>
2024-04-09 01:41:39 -0600

    push

--- app/components/header.js
+++ app/components/header.js
@@ -16,14 +16,15 @@ export default function Header() {
                 <Image src={logo} priority="false" width="300px" alt="Music World Logo" />                 
                    </a>
                    <nav class="ml-6 space-x-2 lg:block">
-                   <button type="button" href='/' class="relative w-1/2 bg-purple-800 shadow-sm text-white rounded-md m-1 py-2 text-sm font-medium whitespace-nowrap focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-opacity-50 focus:z-10 sm:w-auto sm:px-8">Chart</button>
-                   <button type="button" href='/' class="relative w-1/2 bg-purple-800 shadow-sm text-white rounded-md m-1 py-2 text-sm font-medium whitespace-nowrap focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-opacity-50 focus:z-10 sm:w-auto sm:px-8">Artists</button>
-                   <button type="button" href='/' class="relative w-1/2 bg-purple-800 shadow-sm text-white rounded-md m-1 py-2 text-sm font-medium whitespace-nowrap focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-opacity-50 focus:z-10 sm:w-auto sm:px-8">Tracks</button>
-                   <button type="button" href='/' class="relative w-1/2 bg-purple-800 shadow-sm text-white rounded-md m-1 py-2 text-sm font-medium whitespace-nowrap focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-opacity-50 focus:z-10 sm:w-auto sm:px-8">Votes</button>
-                   
+                    <button type="button" href='/' class="relative w-1/2 bg-white shadow-sm border-2 border-purple-800 text-purple-800 rounded-md m-1 py-2 text-s font-medium whitespace-nowrap focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-opacity-50 focus:z-10 sm:w-auto sm:px-8">Chart</button>
+                    <button type="button" href='/' class="relative w-1/2 bg-white shadow-sm border-2 border-purple-800 text-purple-800 rounded-md m-1 py-2 text-s font-medium whitespace-nowrap focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-opacity-50 focus:z-10 sm:w-auto sm:px-8">Artists</button>
+                    <button type="button" href='/' class="relative w-1/2 bg-white shadow-sm border-2 border-purple-800 text-purple-800 rounded-md m-1 py-2 text-s font-medium whitespace-nowrap focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-opacity-50 focus:z-10 sm:w-auto sm:px-8">Tracks</button>
+                    <button type="button" href='/' class="relative w-1/2 bg-white shadow-sm border-2 border-purple-800 text-purple-800 rounded-md m-1 py-2 text-s font-medium whitespace-nowrap focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-opacity-50 focus:z-10 sm:w-auto sm:px-8">Votes</button>
                   </nav>
                   </div><div class="flex justify-end space-x-8">
-                    <button type="button" href='/' class="relative w-1/2 bg-purple-800 shadow-sm text-white rounded-md m-1 py-2 text-sm font-medium whitespace-nowrap focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-opacity-50 focus:z-10 sm:w-auto sm:px-8">Login</button></div></div></div>   
+                  <button type="button" href='/' class="relative w-1/2 bg-purple-800  shadow-sm text-white rounded-md m-1 py-2 text-s font-medium whitespace-nowrap focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-opacity-50 focus:z-10 sm:w-auto sm:px-8">Login</button>
+                   
+                    </div></div></div>   
         
         </div>  
         </>                
